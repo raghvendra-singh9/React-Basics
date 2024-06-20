c3dee46e5dbc5e3aabdc6fe542b156ecda97b630
Raghvendra Singh Shekhawat <163868970+raghvendra-singh9@users.noreply.github.com>
2024-06-20 23:52:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!!</Link>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!!</Link>
+        </header>
 
-      {/* <Pet animal="Dog" name="Luna" breed="Havanese" />
+        {/* <Pet animal="Dog" name="Luna" breed="Havanese" />
       <Pet animal="Bird" name="Pepper" breed="Cokatiel" />
       <Pet animal="Cat" name="Doink" breed="Mixed" /> */}
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
